bbf7a67f81c6d1e5ea806a2fb73301920a15f884
LLPROG <lucalobianco@hotmail.it>
2022-03-24 17:26:52 +0100

    add loader

--- src/components/MainDischi.vue
+++ src/components/MainDischi.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="back">
-    <div class="cards container">
+    <div v-if="cards == null" class="loader"></div>
+    <div v-else class="cards container">
       <CardDischi v-for="card in cards" :key="card.id"
         :cards-data="card"
       />
@@ -22,19 +23,20 @@ export default {
     CardDischi
   },
   created () {
-    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-      .then((response) => {
-        console.log(this.cards)
-        this.cards = response.data.response.map((el) => {
-          return {
-            image: el.poster,
-            title: el.title,
-            author: el.author,
-            year: el.year
-          }
+    setTimeout(() => {
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((response) => {
+          console.log(this.cards)
+          this.cards = response.data.response.map((el) => {
+            return {
+              image: el.poster,
+              title: el.title,
+              author: el.author,
+              year: el.year
+            }
+          })
         })
-      })
-    console.log(this.cards)
+    }, 3000)
   }
 }
 </script>
@@ -43,7 +45,34 @@ export default {
 .back {
   background-color: #1E2D3B;
   padding: 5rem 0;
-
+  .loader {
+    position: relative;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%);
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+    &:after {
+      content: " ";
+      display: block;
+      width: 64px;
+      height: 64px;
+      margin: 8px;
+      border-radius: 50%;
+      border: 6px solid #fff;
+      border-color: #fff transparent #fff transparent;
+      animation: spin 1.2s linear infinite;
+    }
+  }
+}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 .cards {
   display: flex;
